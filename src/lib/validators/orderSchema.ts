c0a6512df07eb033e4ba37a244dc8044438ad7bb
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-09-20 21:04:34 +0530

    place order form

---
+++ src/lib/validators/orderSchema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+//using zod for schema validation
+export const orderSchema = z.object({
+  pincode: z.string({ message: "pincode  should be a string" }).length(6, "pincode  should be 6 chars long"),
+  qty: z.number({ message: "Qty  should be a number" }),
+  productId: z.number({ message: "Product id should be a number" }),
+  address: z.string({ message: "Address should be a string" }).min(5, "Address should be atleast 5 chars long"),
+});
